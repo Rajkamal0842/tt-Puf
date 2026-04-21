f3770ba38118be11b7a8831194cc0fd04f00d873
RajKamal <nemalipurirajkamal@gmail.com>
2026-04-21 15:06:59 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,24 +2,16 @@
 `timescale 1ns / 1ps
 
 module ro_cell (
-    input  wire i_enable,  
-    output wire o_osc_out  
+    input  wire i_enable,
+    output wire o_osc_out
 );
-
-    (* keep = "true" *) wire w_stage_1_nand;
-    (* keep = "true" *) wire w_stage_2_inv;
-    (* keep = "true" *) wire w_stage_3_inv;
-    (* keep = "true" *) wire w_stage_4_inv;
-    (* keep = "true" *) wire w_stage_5_inv;
-
-    assign w_stage_1_nand = ~(i_enable & w_stage_5_inv); 
-    assign w_stage_2_inv  = ~w_stage_1_nand;
-    assign w_stage_3_inv  = ~w_stage_2_inv;
-    assign w_stage_4_inv  = ~w_stage_3_inv;
-    assign w_stage_5_inv  = ~w_stage_4_inv;
-
-    assign o_osc_out = w_stage_5_inv;
-
+    (* keep = "true" *) wire w1, w2, w3, w4, w5;
+    assign w1 = ~(i_enable & w5); 
+    assign w2 = ~w1;
+    assign w3 = ~w2;
+    assign w4 = ~w3;
+    assign w5 = ~w4;
+    assign o_osc_out = w5;
 endmodule
 
 module Puf (
@@ -30,119 +22,43 @@ module Puf (
     output reg        o_done,
     output wire [5:0] o_debug
 );
-
-    wire [3:0] w_sel_ro_a = i_challenge[3:0];
-    wire [3:0] w_sel_ro_b = i_challenge[7:4];
-    
-    wire [15:0] w_ro_outputs; 
-    wire        w_clk_domain_a;
-    wire        w_clk_domain_b;
-
-    reg r_ro_enable;             
-    reg r_clear_counters;      
-
-    localparam STATE_IDLE    = 3'd0;
-    localparam STATE_CLEAR   = 3'd1;
-    localparam STATE_EVAL    = 3'd2;
-    localparam STATE_SETTLE  = 3'd3;
-    localparam STATE_COMPARE = 3'd4;
-    localparam STATE_DONE    = 3'd5;
-
-    reg [2:0] r_current_state;
-    
-    localparam EVAL_WINDOW_MAX = 8'd200; 
-    reg [7:0] r_eval_timer;
-    reg [7:0] r_prev_challenge;
+    wire [3:0] w_sel_a = i_challenge[3:0];
+    wire [3:0] w_sel_b = i_challenge[7:4];
+    wire [15:0] w_ros;
+    wire clk_a, clk_b;
+    reg r_en, r_clr;
+    reg [2:0] state;
+    reg [7:0] timer;
 
     genvar i;
     generate
-        for (i = 0; i < 16; i = i + 1) begin : gen_ro_bank
-            ro_cell inst_ro (
-                .i_enable  (r_ro_enable),
-                .o_osc_out (w_ro_outputs[i])
-            );
+        for (i = 0; i < 16; i = i + 1) begin : ro_gen
+            ro_cell inst (.i_enable(r_en), .o_osc_out(w_ros[i]));
         end
     endgenerate
 
-    assign w_clk_domain_a = w_ro_outputs[w_sel_ro_a];
-    assign w_clk_domain_b = w_ro_outputs[w_sel_ro_b];
+    assign clk_a = w_ros[w_sel_a];
+    assign clk_b = w_ros[w_sel_b];
 
-    reg [7:0] r_count_a;
-    reg [7:0] r_count_b;
+    reg [7:0] cnt_a, cnt_b;
+    always @(posedge clk_a or posedge r_clr) if (r_clr) cnt_a <= 0; else cnt_a <= cnt_a + 1;
+    always @(posedge clk_b or posedge r_clr) if (r_clr) cnt_b <= 0; else cnt_b <= cnt_b + 1;
 
-    always @(posedge w_clk_domain_a or posedge r_clear_counters) begin
-        if (r_clear_counters) r_count_a <= 8'b0;
-        else                  r_count_a <= r_count_a + 1;
-    end
-
-    always @(posedge w_clk_domain_b or posedge r_clear_counters) begin
-        if (r_clear_counters) r_count_b <= 8'b0;
-        else                  r_count_b <= r_count_b + 1;
-    end
-
-    assign o_debug = r_count_a[7:2];
+    assign o_debug = cnt_a[7:2];
 
     always @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
-            r_current_state  <= STATE_IDLE;
-            r_ro_enable      <= 1'b0;
-            r_clear_counters <= 1'b1;
-            o_response       <= 1'b0;
-            o_done           <= 1'b0;
-            r_eval_timer     <= 8'd0;
-            r_prev_challenge <= 8'd0;
+            state <= 0; r_en <= 0; r_clr <= 1; o_done <= 0;
         end else begin
-            if (i_challenge != r_prev_challenge && r_current_state == STATE_DONE) begin
-                r_current_state <= STATE_IDLE; 
-            end
-
-            case (r_current_state)
-                STATE_IDLE: begin
-                    o_done           <= 1'b0;
-                    r_ro_enable      <= 1'b0;
-                    r_clear_counters <= 1'b1; 
-                    r_prev_challenge <= i_challenge;
-                    r_current_state  <= STATE_CLEAR;
-                end
-
-                STATE_CLEAR: begin
-                    r_clear_counters <= 1'b0; 
-                    r_eval_timer     <= 8'd0;
-                    r_current_state  <= STATE_EVAL;
-                end
-
-                STATE_EVAL: begin
-                    r_ro_enable <= 1'b1; 
-                    if (r_eval_timer >= EVAL_WINDOW_MAX) begin
-                        r_ro_enable     <= 1'b0; 
-                        r_current_state <= STATE_SETTLE;
-                    end else begin
-                        r_eval_timer <= r_eval_timer + 1;
-                    end
-                end
-
-                STATE_SETTLE: begin
-                    r_current_state <= STATE_COMPARE;
-                end
-
-                STATE_COMPARE: begin
-                    if (r_count_a > r_count_b) begin
-                        o_response <= 1'b1;
-                    end else begin
-                        o_response <= 1'b0;
-                    end
-                    r_current_state <= STATE_DONE;
-                end
-
-                STATE_DONE: begin
-                    o_done <= 1'b1; 
-                end
-
-                default: r_current_state <= STATE_IDLE;
+            case (state)
+                0: begin r_clr <= 0; timer <= 0; state <= 1; end
+                1: begin r_en <= 1; if (timer == 200) state <= 2; else timer <= timer + 1; end
+                2: begin r_en <= 0; state <= 3; end
+                3: begin o_response <= (cnt_a > cnt_b); o_done <= 1; state <= 4; end
+                4: state <= 4;
             endcase
         end
     end
-
 endmodule
 
 module tt_um_puf (
@@ -155,17 +71,16 @@ module tt_um_puf (
     input  wire       clk,      
     input  wire       rst_n     
 );
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
-    assign uio_out = 8'b00000000;
-    assign uio_oe  = 8'b00000000;
-
-    Puf core_puf_inst (
-        .i_clk       (clk),
-        .i_rst_n     (rst_n),
-        .i_challenge (ui_in),        
-        .o_response  (uo_out[0]),    
-        .o_done      (uo_out[1]),    
-        .o_debug     (uo_out[7:2])   
+    // This part was missing in your screenshot!
+    Puf core (
+        .i_clk(clk),
+        .i_rst_n(rst_n),
+        .i_challenge(ui_in),
+        .o_response(uo_out[0]),
+        .o_done(uo_out[1]),
+        .o_debug(uo_out[7:2])
     );
-
 endmodule
