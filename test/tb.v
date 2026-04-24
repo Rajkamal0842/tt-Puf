c4141429db08cc4a98b463ab1682e54b0b438847
RajKamal <nemalipurirajkamal@gmail.com>
2026-04-25 05:04:01 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,5 +1,6 @@
 `default_nettype none
 `timescale 1ns / 1ps
+
 module tb ();
   initial begin
     $dumpfile("tb.fst");
@@ -23,17 +24,10 @@ module tb ();
 
   tt_um_puf user_project (
 `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
+      .VPWR(VPWR), .VGND(VGND),
 `endif
-      .ui_in  (ui_in),
-      .uo_out (uo_out),
-      .uio_in (uio_in),
-      .uio_out(uio_out),
-      .uio_oe (uio_oe),
-      .ena    (ena),
-      .clk    (clk),
-      .rst_n  (rst_n)
+      .ui_in(ui_in), .uo_out(uo_out),
+      .uio_in(uio_in), .uio_out(uio_out), .uio_oe(uio_oe),
+      .ena(ena), .clk(clk), .rst_n(rst_n)
   );
-
 endmodule
