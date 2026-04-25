c48b5fbc3f71d92264a846935adf68fb0dd0e740
RajKamal <nemalipurirajkamal@gmail.com>
2026-04-25 06:31:46 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,13 +1,11 @@
 `default_nettype none
 `timescale 1ns / 1ps
-
 module tb ();
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
     #1;
   end
-
   reg        clk;
   reg        rst_n;
   reg        ena;
@@ -16,18 +14,22 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
-
   tt_um_puf user_project (
 `ifdef GL_TEST
-      .VPWR(VPWR), .VGND(VGND),
+      .VPWR(VPWR),
+      .VGND(VGND),
 `endif
-      .ui_in(ui_in), .uo_out(uo_out),
-      .uio_in(uio_in), .uio_out(uio_out), .uio_oe(uio_oe),
-      .ena(ena), .clk(clk), .rst_n(rst_n)
+      .ui_in  (ui_in),
+      .uo_out (uo_out),
+      .uio_in (uio_in),
+      .uio_out(uio_out),
+      .uio_oe (uio_oe),
+      .ena    (ena),
+      .clk    (clk),
+      .rst_n  (rst_n)
   );
 endmodule
